2c505f85bcac88b3af918a06dfb69bf101d32d5e
苏敏 <12146385+sumin194610@user.noreply.gitee.com>
2023-08-24 11:28:33 +0800

    su

---
+++ src/main/java/com/lxc/service/ManageService.java
@@ -0,0 +1,13 @@
+package com.lxc.service;
+
+import com.lxc.entity.Manager;
+
+/**
+ * @Description: TODO
+ * @Author: SuMin
+ * @Date: 2023/8/24
+ **/
+public interface ManageService {
+
+    public Manager manageLogin(String managername, String managerpwd);
+}
