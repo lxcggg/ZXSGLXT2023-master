b24f33af274195e433b90ac772edfb4947081f88
lxc14 <1447234802@qq.com>
2023-08-24 09:29:20 +0800

    2023/8/24

--- src/main/java/com/lxc/config/MvcConfig.java
+++ src/main/java/com/lxc/config/MvcConfig.java
@@ -23,10 +23,10 @@ public class MvcConfig implements WebMvcConfigurer {
         WebMvcConfigurer adapter = new WebMvcConfigurer() {
             @Override
             public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/share.html").setViewName("Login");
                 registry.addViewController("/index.html").setViewName("Login");
                 registry.addViewController("/main.html").setViewName("Login2");
                 registry.addViewController("/tables.html").setViewName("Tables");
+                registry.addViewController("/user.html").setViewName("userList");
             }
 
             //注册拦截器
